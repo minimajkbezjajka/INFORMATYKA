a5ef12fe28167dc1f1d4244699a76369b13c68f7
minimajkbezjajka <dominikgr08@gmail.com>
2024-11-28 09:06:51 +0100

    while

---
+++ while/whilezad1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int number = 2;
+
+ while (number <= 10){
+  cout << number << " ";
+    number += 2;
+ 
+ }
+ 
+   
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
