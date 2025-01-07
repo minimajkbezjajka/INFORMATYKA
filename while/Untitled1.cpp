cc5b8f209bb823eb841cfca5f1677bea88413ac1
minimajkbezjajka <dominikgr08@gmail.com>
2025-01-07 10:36:24 +0100

    zad3

---
+++ while/Untitled1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	for(int i = 0; i <= 8; i += 2)
+{
+ 
+    cout << "Liczba parzysta: " << i << endl;
+}
+
+}
+
---
+++ while/okokok.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int suma;
+	suma=0;
+cout << "Podaj liczbe";
+	int N;
+	cin>> N;
+	for(int i = 1; i <= N; i++)
+{
+cout << i;
+suma= i + suma;
+ 
+	}	
+	cout << " liczba od 1 do " << N <<"wynosi " << suma;
+
+	
+	
+	
+	
+	
+	
+}
