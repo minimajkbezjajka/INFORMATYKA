130e87d66eb45feadf36cb0143b5caa9f6919c4a
minimajkbezjajka <dominikgr08@gmail.com>
2025-01-07 10:56:49 +0100

    pzdr

--- for/FORZADANIE6.cpp
+++ for/FORZADANIE6.cpp
@@ -1,30 +1,26 @@
 #include <iostream>
 using namespace std;
- 
+
 int main() {
     int szerokosc, wysokosc;
- 
-    cout << "Podaj szerokosc dywanu: ";
+
+    cout << "Podaj szerokosc prostokata: ";
     cin >> szerokosc;
-    cout << "Podaj wysokosc dywanu: ";
+    cout << "Podaj wysokosc prostokata: ";
     cin >> wysokosc;
- 
+
+
     for (int i = 0; i < wysokosc; i++) {
         for (int j = 0; j < szerokosc; j++) {
-            if (i == 0 || i == wysokosc - 1) {
-                cout << "-";
-            }
-            else if (j == 0 || j == szerokosc - 1) {
-                cout << "|";
-            }
-            else if ((i + j) % 2 == 0) {
-                cout << "#";
-            } else {
+     
+            if (i == 0 || i == wysokosc - 1 || j == 0 || j == szerokosc - 1) {
                 cout << "*";
+            } else {
+                cout << " ";
             }
         }
-        cout << endl;
+        cout << endl; 
     }
- 
+
     return 0;
 }
